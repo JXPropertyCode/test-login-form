e4eac6744f177453f2acd6b264f20315b3d70898
JXPropertyCode <yellowdice92@hotmail.com>
2021-08-17 15:54:02 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// usually this should be stored in a server/DB
+	const adminUser = {
+		email: "admin@admin.com",
+		password: "123",
+	};
+
+	const [user, setUser] = useState({ name: "", email: "" });
+	// catch if the login is correct
+	const [error, setError] = useState(false);
+
+	const login = (details) => {
+		console.log(details);
+		if (
+			details.email === adminUser.email &&
+			details.password === adminUser.password
+		) {
+			console.log("Logged In");
+			setUser({
+				name: details.name,
+				email: details.email,
+			});
+      setError(false)
+		} else {
+			console.log("Login Details Do Not Match");
+      setError(true)
+		}
+	};
+
+	const logout = () => {
+		console.log("Loutout");
+		setUser({ name: "", email: "" });
+	};
+
+	return (
+		<div className="App">
+			{/* ternary operator */}
+			{user.email !== "" ? (
+				<div className="welcome">
+					<h2>
+						Welcome, <span>{user.name}</span>
+					</h2>
+					<button onClick={logout}>Logout</button>
+				</div>
+			) : (
+				<LoginForm login={login} error={error} />
+			)}
+		</div>
+	);
 }
 
 export default App;
